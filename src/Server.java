6fae3af1280f6f8d56f102879d49f83aac064a58
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-18 10:24:56 +0100

    created a server socket on port 5000 and application is accepting input from console (for now)

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,22 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Server {
+	public static void main(String args[]) {
+		ServerSocket s = null;
 
+		//Input From Keyboard
+		DataInputStream indata = new  DataInputStream (System.in);
+		System.out.println("Type in Something & Press Enter to Send it To The >>C L I E N T<<: ");
+		
+		
+		// Register your service on port 5432
+		try {
+			s = new ServerSocket(5432);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+	}
 }
