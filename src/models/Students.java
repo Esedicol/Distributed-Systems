7d62b1434947b8795beeeec9479392f2ecc3a08e
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-11-07 18:34:07 +0000

    Previous commit contained an empty class so this is a re-push

--- src/models/Students.java
+++ src/models/Students.java
@@ -1,5 +1,50 @@
 package models;
 
-public class Students {
+
+class Student {
+
+    private String sid;
+    private String stud_id;
+    private String fname;
+    private String sname;
+    
+	public Student(String sid, String stud_id, String fname, String sname) {
+		this.sid = sid;
+		this.stud_id = stud_id;
+		this.fname = fname;
+		this.sname = sname;
+	}
+
+	public String getSid() {
+		return sid;
+	}
+
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+
+	public String getStud_id() {
+		return stud_id;
+	}
+
+	public void setStud_id(String stud_id) {
+		this.stud_id = stud_id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
 
 }
