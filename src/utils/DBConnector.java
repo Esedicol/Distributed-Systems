2a265e42c5ed5cb8670ca21fbb5736ff8e146af1
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-11-07 18:31:33 +0000

    Added model for students

--- src/utils/DBConnector.java
+++ src/utils/DBConnector.java
@@ -1,5 +1,30 @@
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/*
+ * @author Emmanuel Sedicol
+ */
+
 public class DBConnector {
+	
+	public DBConnector() {
+	}
+
+	// *********** Connection Method *********** //
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		Properties connectionProps = new Properties();
+		connectionProps.put("user", "esedicol");
+		connectionProps.put("password", "krlafe8IM");
+
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/BSCY4",connectionProps);
+		return conn;
+	}
+	
+	
 
 }
